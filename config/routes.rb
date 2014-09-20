e0c25a5e5b63b7a51feb859e89f8c94fe17b0c59
Neil Serebryany <neilssurf@gmail.com>
2014-09-20 17:16:07 -0400

    let's see

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  root 'pages/home'
-
-  get 'pages/search'
+match '/home' => 'pages#home'
+match '/search' => 'pages#search'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
