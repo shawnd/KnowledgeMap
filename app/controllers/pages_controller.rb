44e210b229b8156fb305a54fa4396715e237b752
Allen <allendinghamilton@gmail.com>
2014-09-21 04:15:49 -0400

    search controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,19 @@
-class PagesController < ApplicationController
+ class PagesController < WikiparseController
+
   def home
   end
 
   def search
+  	
+  	searchString = params[:search]
+
+  	node = Entry.where(node: searchString).take
+
+  	if node == nil
+  		# WikiparseController.parseSearch
+  	else 
+  		nodes = Entry.where(node: searchString, type: "child").take(5)
+  	end
+  		
   end
 end
